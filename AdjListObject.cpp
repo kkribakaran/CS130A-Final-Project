629f18e0c4cf94586437b447184e57273680d1c5
Karthik <kkribakaran@umail.ucsb.edu>
2016-02-20 21:10:26 -0800

    Added AdjListObject and AdjacencyList classes

--- AdjListObject.cpp
+++ AdjListObject.cpp
@@ -2,32 +2,32 @@
 #include <string>
 
 AdjListObject::AdjListObject() {
-name = "";
-profileDataIndex = -10;
-root = NULL;
+  name = "";
+  profileDataIndex = -10;
+  root = NULL;
 }
 
-std::string getName() {
-return name;
+std::string AdjListObject::getName() {
+  return name;
 }
 
-int getProfileDataIndex() {
-return int;
+int AdjListObject::getProfileDataIndex() {
+  return int;
 }
-void setName(std::string name) {
-this->name = name; 
+void AdjListObject::setName(std::string name) {
+  this->name = name; 
 }
-void setProfileDataIndex(int index) {
-this->profileDataIndex = index;
+void AdjListObject::setProfileDataIndex(int index) {
+  this->profileDataIndex = index;
 }
-void addFriend(std::string friend) {
-Node* temp = root;
-Node* friendNode;
-friendNode.name = friend;
-friendNode.next = NULL; 
-while (temp->next) {
-temp = temp->next;
-}
-temp->next = friendNode;
+void AdjListObject::addFriend(std::string friend) {
+  Node* temp = root;
+  Node* friendNode;
+  friendNode.name = friend;
+  friendNode.next = NULL; 
+  while (temp->next) {
+    temp = temp->next;
+  }
+  temp->next = friendNode;
 }
 
