09c327ff250de5a3ee9de678d6102c8b024cf9de
Marvin Shu <marvinshu@umail.ucsb.edu>
2016-02-27 17:46:20 -0800

    Copied over main program and edited it for our design

--- main.cpp
+++ main.cpp
@@ -1,16 +1,43 @@
 #include "AdjListObject.h"
+#include "AdjacencyList.h"
+
+#include <sstream>
+#include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+vector<string> split(string str, char delimiter)
+{
+	vector<string> internal;
+	stringstream ss(str);       // turn the string into a stream.
+	string tok;
+
+	while(getline(ss, tok, delimiter))
+	{
+		internal.push_back(tok);
+	}
+
+	return internal;
+}
 int main()
 {
-  AdjListObject steve("Steve", 5);
-  steve.addFriend("Dave");
-  steve.addFriend("Tammy");
-  steve.addFriend("Bob");
+  AdjacencyList list;
 
-  for(AdjListObject::FriendNode* p = steve.getRoot(); p!= NULL; p = p->next)
-    cout<<p->name<<endl;
-
+  ifstream f;
+  f.open("input.txt", ios::in);
+  if(!f) cerr << "File not found" << endl;
+  else
+    {
+      string line;
+      while(std::getline(f, line))
+	{
+	  vector<string> words = split(line, ',');
+	  vector<string> friends;
+	  for(int i = 3; i < (int)words.size(); i++)
+	    friends.push_back(words.at(i));
+	  list.insert(words[0],words[1],words[2],friends,friends.size());
+	}
+    }
   return 0;
 }
