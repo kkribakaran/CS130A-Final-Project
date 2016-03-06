73438ebe50720fb8170e8b707ab6cae00dbbc3f7
Karthik <kkribakaran@umail.ucsb.edu>
2016-03-05 16:48:52 -0800

    Command line input supported (no range query

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <stdio.h>
+
 using namespace std;
 
 vector<string> split(string str, char delimiter)
@@ -20,6 +22,7 @@ vector<string> split(string str, char delimiter)
 
 	return internal;
 }
+
 int main(int argc, char* argv[])
 {
   if(argc < 2)
@@ -27,8 +30,18 @@ int main(int argc, char* argv[])
       cout<<"Please input a text file"<<endl;
       return 0;
     }
+  
   AdjacencyList list;
+
+  //if ProfileData already exists, delete it. 
+  FILE* profileData;
+  profileData = fopen("ProfileData.txt","r");
+  if (profileData)
+    std::remove("ProfileData.txt");
+
+
   ifstream f;
+
   f.open(argv[1], ios::in);
   
   if(!f)
@@ -42,8 +55,53 @@ int main(int argc, char* argv[])
 	  vector<string> friends;
 	  for(int i = 3; i < (int) words.size(); i++)
 	    friends.push_back(words.at(i));
-	  list.insert(words.at(0),words.at(1),words.at(2),friends,(int)friends.size());
+	  list.initInsert(words.at(0),words.at(1),words.at(2),friends,(int)friends.size());
 	}
     }
+    while (true) 
+    {
+      std::string input = "";
+      std::string command = "";
+      getline(cin,input);
+      istringstream iss(input);
+      iss>>command;
+      vector<string> parameters;
+      string currentParam = "";
+      while (iss>>currentParam)
+	parameters.push_back(currentParam);
+      
+      if (command == "Insert")
+	{
+	  string name = parameters.at(0);
+	  string age = parameters.at(1);
+	  string occupation = parameters.at(2);
+	  list.insert(name,age,occupation);			   
+	 
+	} 
+      else if (command == "AddFriend") 
+	{
+	  string friend1 = parameters.at(0);
+	  string friend2 = parameters.at(1);
+	  list.addFriend(friend1,friend2);
+	  list.listFriendsInfo(friend1);
+	  list.listFriendsInfo(friend2);
+	}
+      else if (command == "ListFriendsInfo")
+        {
+          string name  = parameters.at(0);
+          list.listFriendsInfo(name);
+	}
+      else if (command == "PrintAll")
+        {
+	  list.printAll();
+	}
+
+      else
+	    {
+	  cout<<"Invalid Command."<<endl;
+	  break;
+	}
+    }
+  
   return 0;
 }
